df79c3e81f39b2a69e0d3b3fcb828ae4a5dfb206
Pickchr2 <Cpickens1270@gmail.com>
2023-02-12 18:46:11 -0700

    Add accumelate messages function

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -1,17 +1,27 @@
+'Christopher Pickens
+'RCET0265
+'Spring 2023
+'Accumulate Messages
+'https://github.com/Pickchr2/AccumulateMessages.git
 
 Imports System
 
 Module MessageProgram
     Sub Main(args As String())
         'uncomment to test interactively
-        'Test.Manual()
-        Test.Auto()
+        Test.Manual()
+        'Test.Auto()
     End Sub
 
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
         'your code here
+
+        Static messages As String
+        If clear Then
+            messages = ""
+        Else
+            messages += newMessage + vbCrLf
+        End If
         Return messages
     End Function
-
-
 End Module
